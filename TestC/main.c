1382c3e87805e18ecc1495f892604448447b0bb6
DrVidjet <147281668+DrVidjet@users.noreply.github.com>
2023-11-01 18:34:41 +0400

    1

--- TestC/main.c
+++ TestC/main.c
@@ -4,37 +4,23 @@
 
 int main()
 {
-    double x1,x2,y1,y2,x,y,A,B,C,rez;
+    double s, d;
+    int n, i, j;
 
-    printf("Enter x1 and y1: ");
-    scanf("%lf %lf", &x1, &y1);
-    printf("Enter x2 and y2: ");
-    scanf("%lf %lf", &x2, &y2);
-    if(x1 > 0 && y1 > 0 && x2 > 0 && y2 > 0 ||
-       x1 < 0 && y1 > 0 && x2 < 0 && y2 > 0 ||
-       x1 < 0 && y1 < 0 && x2 < 0 && y2 < 0 ||
-       x1 < 0 && y1 > 0 && x2 < 0 && y2 > 0)
+    printf("Enter n: ");
+    scanf("%d", &n);
+    s = 0;
+
+    for(i = 1; i <= n; i++)
     {
-    A = hypotf(x1, y1);
-    B = hypotf(x2, y2);
-
-    x = fabs(x1)-fabs(x2);
-    y = fabs(y1)-fabs(y2);
-
-    C = hypotf(x, y);
-    rez = A+B+C;
-
-    printf("A = %lf, B = %lf, C = %lf\n", A, B, C);
-    printf("Result = %lf\n", rez);
+        d = i; j++)
+            d *= i
+        for(j = 1; j < i;;
+        s += 1/d;
+        //printf("d = %d\n", d);
     }
-    else if( x1 == x2 || y1 == y2 )
-        printf("Oni lezat na odnoi pramoi\n");
-    else if (x1 > 0 && y1 > 0 && x2 < 0 && y2 > 0 ||
-             x1 < 0 && y1 < 0 && x2 > 0 && y2 < 0 ||
-             x1 > 0 && y1 > 0 && x2 > 0 && y2 < 0 ||
-             x1 < 0 && y1 < 0 && x2 < 0 && y2 > 0 )
-        printf("Oni lezat na odnoi polu-ploskosti\n");
 
+    printf("s = %d\n", s);
 
     return 0;
 }
