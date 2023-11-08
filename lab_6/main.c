9f8e9f0fe4b326697c77b538137107148f2550a6
DrVidjet <147281668+DrVidjet@users.noreply.github.com>
2023-11-08 12:54:07 +0400

    1

--- lab_6/main.c
+++ lab_6/main.c
@@ -1,1 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
 
+int end;
+
+int main(void)
+{
+    int choice;
+
+    printf("Enter the program number(1-3): ");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+        case 1: first();
+        break;
+        case 2: second();
+        break;
+    }
+
+    system("pause");
+}
+
+
+
+int choice()
+{
+    int choice;
+    printf("1 - repeat the program, 2 - go back to the menu, 3 - exit the program\n");
+    scanf("%d", &choice);
+    if(choice == 1)
+    {
+       switch(end)
+       {
+           case 1: first();
+           break;
+           case 2: second();
+           break;
+       }
+    }
+    else if(choice == 2)
+        main();
+    else if(choice == 3)
+        system("exit");
+}
