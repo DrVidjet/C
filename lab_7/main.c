684fa32385344a422051b67efe3dc606a13f9f98
DrVidjet <147281668+DrVidjet@users.noreply.github.com>
2023-11-14 10:58:54 +0400

    1

--- lab_7/main.c
+++ lab_7/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <string.h>
+#include <stdbool.h>
 
 int end;
 
@@ -30,23 +32,70 @@ void first()
 {
     char *filename;
     filename = (char*)malloc(15);
-    int i, c;
+    int i, j, c = 0, d = 0, b = 0;
+    bool prov;
 
+    printf("Enter the file name: ");
     fflush(stdin);
     scanf("%s", filename);
 
     FILE *fp = fopen(filename, "r+");
-    char buffer[256];
+    char *buffer;
+    char *postbuffer;
+    postbuffer = (char*)malloc(256);
+    buffer = (char*)malloc(256);
 
     if(fp)
     {
         while((fgets(buffer, 256, fp)) != NULL)
         {
-            printf("%s", buffer);
-            fclose(fp);
-            printf("\nWritten!\n");
-        }
+            buffer = realloc(buffer, strlen(buffer));
+            postbuffer = realloc(postbuffer, strlen(buffer));
+            for(i = 0; i < strlen(buffer); i++)
+            {
+                if(buffer[i] == 'o' || buffer[i] == 'O')
+                {
+                    c++;
+                }
+                else if(buffer[i] == ' ')
+                {
+                    if(c > 1)
+                        prov = 1;
+                    c = 0;
+                }
+                else if(buffer[i] == '.')
+                {
+                    if(prov == 0)
+                    {
+                        for(j = d; j < i; j++)
+                        {
+                            postbuffer[b] = buffer[j+2];
+                            //printf("buffer[%d] = %c, postbuffer[%d] = %c\n", j, buffer[j], b, postbuffer[b]);
+                            b++;
+                        }
+                    }
+                    prov = 0;
+                    d = i;
+                }
+            }
 
+
+            postbuffer = realloc(postbuffer, b);
+            //Hollow knight. Hello knight.
+            fclose(fp);
+            remove(filename);
+            FILE *fp = fopen(filename, "w+");
+            fprintf(fp, buffer);
+            printf("Before:\n\n%s\n\n", buffer);
+
+            printf("After:\n\n");
+            for(i = 0; i < b; i++)
+                printf("%c", postbuffer[i]);
+            printf("\n\n");
+        }
+    }
+
+    free(postbuffer);
     free(filename);
     end = 1;
     choice();
